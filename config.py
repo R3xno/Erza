8e598b6b5f314c95962892c54fdbb12b3df9fbce
REXNO <rexno6969@gmail.com>
2025-10-05 21:37:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ BOT_TOKEN = "8223753744:AAHAeDCsD0lMu6FZ5-5WA2GrQ64sSUA0WHk"
 MONGO_URI = "mongodb+srv://sofie_music:w5DJQx1NzoGWbYTD@cluster0.eczplfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "Cluster0"
 OWNER_ID = 1101488645
-BOT_USERNAME = "NomadeHelpBot"  # Bot username without @
-SUPPORT_GROUP = "https://t.me/telegram"
+BOT_USERNAME = "Erza_robot"  # Bot username without @
+SUPPORT_GROUP = "https://t.me/+iDjRUpdu-d9jODc1"
 UPDATE_CHANNEL = "https://t.me/telegram"
-START_IMAGE = "https://files.catbox.moe/j2yhce.jpg"
+START_IMAGE = "https://telegra.ph/file/2973391580c8dc8f45eeb-7aff64f665a3a39f72.jpg"
