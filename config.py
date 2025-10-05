06795f4631c05c68782f5bad7d2326a06efbb69c
REXNO <rexno6969@gmail.com>
2025-10-05 21:22:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@
 
 API_ID = 9333070
 API_HASH = "511eb11eda4af78ec8f9a0a7de9e1241"
-BOT_TOKEN = "8472124806:AAHwAJrPaz_LT61Y3oEsv0Z6egT8vyJujmk"
+BOT_TOKEN = "8223753744:AAHAeDCsD0lMu6FZ5-5WA2GrQ64sSUA0WHk"
 MONGO_URI = "mongodb+srv://sofie_music:w5DJQx1NzoGWbYTD@cluster0.eczplfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "Cluster0"
-OWNER_ID = 5016109398
+OWNER_ID = 1101488645
 BOT_USERNAME = "NomadeHelpBot"  # Bot username without @
-SUPPORT_GROUP = "https://t.me/learning_bots"
-UPDATE_CHANNEL = "https://t.me/learningbots79"
+SUPPORT_GROUP = "https://t.me/telegram"
+UPDATE_CHANNEL = "https://t.me/telegram"
 START_IMAGE = "https://files.catbox.moe/j2yhce.jpg"
